51e2bdc0ae3a14aed3983e6fb1896f47c4eb5cbb
paperclip <github@leeder.plus.com>
2018-12-22 18:20:44 +0000

    Script to start screen
    Add a script to start screen

---
+++ startScreen.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+START_DIR=${0%/*}
+[[ -d $START_DIR ]] && cd ${START_DIR}
+
+SESSION_NAME=camera
+screen -S "$SESSION_NAME" -d -m -U
+screen -S "$SESSION_NAME" -p 0 -X python timelapse.py
+screen -S "$SESSION_NAME" -X screen 1
+screen -S "$SESSION_NAME" -p 1 -X python emailClient.py
