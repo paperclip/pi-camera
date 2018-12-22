e9b80e8193be16bb61de3b5ad7b3b30e81bb5708
paperclip <github@leeder.plus.com>
2018-12-22 18:29:57 +0000

    Another try at starting screen

--- startScreen.sh
+++ startScreen.sh
@@ -5,6 +5,5 @@ START_DIR=${0%/*}
 
 SESSION_NAME=camera
 screen -S "$SESSION_NAME" -d -m -U
-screen -S "$SESSION_NAME" -p 0 -X exec python timelapse.py
-screen -S "$SESSION_NAME" -X screen 1
-screen -S "$SESSION_NAME" -p 1 -X exec python emailClient.py
+screen -S "$SESSION_NAME" -t timelapse -X exec python timelapse.py ~/webdata/camera
+screen -S "$SESSION_NAME" -t email -X exec python emailClient.py
