73c47f6fdb3c92f4f3329eb1d0d42dca6770da96
Douglas Leeder <douglas.leeder@gmail.com>
2018-12-22 19:59:59 +0000

    Another attempt to startScreen.sh

--- startScreen.sh
+++ startScreen.sh
@@ -4,6 +4,14 @@ START_DIR=${0%/*}
 [[ -d $START_DIR ]] && cd ${START_DIR}
 
 SESSION_NAME=camera
-screen -S "$SESSION_NAME" -d -m -U
-screen -S "$SESSION_NAME" -t timelapse -X exec python timelapse.py ~/webdata/camera
-screen -S "$SESSION_NAME" -t email -X exec python emailClient.py
+screen -S "$SESSION_NAME" -d -m -U -A 
+# screen -S "$SESSION_NAME" -p 0 -t timelapse -Adm python timelapse.py ~/webdata/camera
+# screen -S "$SESSION_NAME" -t timelapse -p 2 python timelapse.py ~/webdata/camera
+# screen -S "$SESSION_NAME" -t email -Adm python emailClient.py
+# screen -S "$SESSION_NAME" -t email -p 3 python emailClient.py
+
+NL=$(echo -ne '\015')
+screen -S "$SESSION_NAME" -X screen 1
+screen -S "$SESSION_NAME" -p 1 -X stuff "python timelapse.py ~/webdata/camera$NL"
+screen -S "$SESSION_NAME" -X screen 2
+screen -S "$SESSION_NAME" -p 2 -X stuff "python emailClient.py$NL"
