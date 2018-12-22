92175687a75755368c753859eae1303dd69043eb
paperclip <github@leeder.plus.com>
2018-12-22 18:25:03 +0000

    Add exec calls

--- startScreen.sh
+++ startScreen.sh
@@ -5,6 +5,6 @@ START_DIR=${0%/*}
 
 SESSION_NAME=camera
 screen -S "$SESSION_NAME" -d -m -U
-screen -S "$SESSION_NAME" -p 0 -X python timelapse.py
+screen -S "$SESSION_NAME" -p 0 -X exec python timelapse.py
 screen -S "$SESSION_NAME" -X screen 1
-screen -S "$SESSION_NAME" -p 1 -X python emailClient.py
+screen -S "$SESSION_NAME" -p 1 -X exec python emailClient.py
